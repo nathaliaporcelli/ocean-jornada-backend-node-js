71d1d2190790b88cd7c594a89cbdae319138a774
Nathalia Porcelli <57419374+nathaliaporcelli@users.noreply.github.com>
2024-02-27 14:57:45 +0000

    criação de conexão banco

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 const express = require('express')
-const app = express()
+const dbUrl = 'mongodb+srv://admin:gdlMhyeNRuPWW8eZ@cluster0.ifsmzso.mongodb.net'
+const dbName = 'OceanJornadaBackend'
+const { MongoClient } = require('mongodb')
 
-app.get('/',function(req,res){
-    res.send('Hello world')
-})
-
-app.get('/oi',function(req,res){
-    res.send('oieeeeeesssss')
-})
-
-// lista personagens
-const lista = ['Rick Sanchez', 'Morty Smith','Summer Smith']
-app.listen(3000)
-
-//read all get /item
-app.get('/item', function(req,res){
-    res.send(lista)
-})
-
-//read by id
-
-app.get('/item/:id', function(req,res){
-    const id = req.params.id
-    res.send(id)
+async function main() {
+    const client = new MongoClient(dbUrl)
+    console.log('Conectando ao banco de dados..')
+    await client.connect()
+    console.log('conectado')
+    const app = express()
+    app.get('/', function (req, res) {
+        res.send('Hello world')
     })
-// post
 
-app.use(express.json())
+    app.get('/oi', function (req, res) {
+        res.send('oieeeeeesssss')
+    })
 
-app.post('/item',function(req,res){
-    const body = req.body
+    // lista personagens
+    const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
-    const item = body.nome
 
-    lista.push(item)
+    //read all get /item
+    app.get('/item', function (req, res) {
+        res.send(lista)
+    })
 
-    res.send('Item adicionado com sucesso')
-})
+    //read by id
+
+    app.get('/item/:id', function (req, res) {
+        const id = req.params.id
+        res.send(id)
+    })
+    // post
+
+    app.use(express.json())
+
+    app.post('/item', function (req, res) {
+        const body = req.body
+
+        const item = body.nome
+
+        lista.push(item)
+
+        res.send('Item adicionado com sucesso')
+    })
+    app.listen(3000)
+}
+main()
